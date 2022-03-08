089c7b305397decb1b7638309edfe2b10824960b
MEVEN <meven.ameline@gmail.com>
2022-03-08 20:15:58 +0100

    Level transition

--- Assets/Scripts/LoadSpecificScene.cs
+++ Assets/Scripts/LoadSpecificScene.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class LoadSpecificScene : MonoBehaviour
 {
     public string sceneName;
+    public Animator fadeSystem;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            // Load next scene
-            SceneManager.LoadScene(sceneName);
-
+        if (collision.CompareTag("Player")) { 
+            StartCoroutine(TriggerSceneLoad());
         }
+    }
+
+    private IEnumerator TriggerSceneLoad()
+    {
+        fadeSystem.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(.5f);
+        SceneManager.LoadScene(sceneName);
     }
 }
