33d12d38407647040f0a9dbfc9e3710f19556a7a
MEVEN <meven.ameline@gmail.com>
2022-03-22 06:31:44 +0100

    Checkpoint
    Dsactivation des checkpoints déjà visités

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     private Transform playerSpawn;
+    private bool isActive = true;
 
     void Awake()
     {
         Debug.Log("Awake Ckeckpoint");
         playerSpawn = GameObject.FindGameObjectWithTag("Respawn").transform;
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!isActive)
+        {
+            Debug.Log("Checkpoint inactive");
+            return;
+        }
         Debug.Log("OnCollisionEnter");
         if (collision.transform.CompareTag("Player"))
         {
             Debug.Log("OnCollisionEnter Player");
             playerSpawn.position = transform.position;
+            isActive = false;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+
         }
         
     }
